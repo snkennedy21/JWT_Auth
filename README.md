7d646745903e7ffcd80646b53bd7c57da92a6436
Sean Kennedy <seankennedy@Seans-MacBook-Pro-2.local>
2023-09-10 12:26:45 -0400

    Add email, first_name, last_name, password, created_at columns to User table

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JWT_Auth
 Repository to create JWT tokens and Refresh Tokens
+
+
+# Run Application In Detached Moded
+docker-compose up -d
+
+# Make A Migration File
+docker-compose run api alembic revision --autogenerate -m "Your Message Here"
+
+# Migrate Changes To Postgres Database
+docker-compose run api alembic upgrade head
--- app/models.py
+++ app/models.py
@@ -8,10 +8,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-
-class OtherUser(Base):
-    __tablename__ = "other_users"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
