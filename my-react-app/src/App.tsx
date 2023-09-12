35d5df128c0041147493587e1a8b20bf7bf8444d
Sean Kennedy <seankennedy@Seans-MBP-2.fios-router.home>
2023-09-12 18:27:02 -0400

    Add initial landing page

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,14 +1,14 @@
 import Login from "./Login";
-import NavigationBar from "./NavigationBar";
 import React from "react";
+import Page from "./Page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavigationBar />
         <Routes>
+          <Route path="/" element={<Page />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
