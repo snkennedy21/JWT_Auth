21dfd05fdf480660d367587521267544e397ce5d
Sean Kennedy <seankennedy@Seans-MBP-2.fios-router.home>
2023-09-12 18:30:58 -0400

    Separate navbar into its own component

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,14 @@
 import Login from "./Login";
 import React from "react";
 import Page from "./Page";
+import Navbar from "./Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Page />} />
           <Route path="/login" element={<Login />} />
