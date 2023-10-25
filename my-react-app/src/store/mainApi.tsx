b00329360da9bd909cfba4be11a961c3e48114d8
snkennedy21 <snkennedy21@gmail.com>
2023-10-25 09:10:30 -0400

    Add correct Kubernetes files

--- my-react-app/src/store/mainApi.tsx
+++ my-react-app/src/store/mainApi.tsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const mainApi = createApi({
   reducerPath: "mainApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: "http://react-fastapi-website.com",
   }),
 
   endpoints: (builder) => ({
