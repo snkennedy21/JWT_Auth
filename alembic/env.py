a61ffae3404a5f2904e17eac40d5e819bf38da32
snkennedy21 <snkennedy21@gmail.com>
2023-11-16 12:18:53 -0500

    Fix bug for api startup not waiting for database connection

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from app.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f'postgresql://root:root@db:5432/postgres')
+config.set_main_option("sqlalchemy.url", f'postgresql://username:password@db:5432/postgres')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/database.py
+++ app/database.py
@@ -7,14 +7,30 @@ import time
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/hostname>:<database port>/<database_name>"
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://root:root@db:5432/postgres'
+SQLALCHEMY_DATABASE_URL = f'postgresql://username:password@db:5432/postgres'
 
+max_retries = 5
+retry_interval = 5
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+def create_engine_with_retry(url, max_retries, retry_interval):
+    retry_count = 0
+    while True:
+        try:
+            engine = create_engine(url)
+            engine.connect()
+            return engine
+        except Exception as e:
+            retry_count += 1
+            if retry_count > max_retries:
+                raise
+            print(f"Retrying in {retry_interval} seconds ({retry_count}/{max_retries})...")
+            time.sleep(retry_interval)
+
+engine = create_engine_with_retry(SQLALCHEMY_DATABASE_URL, max_retries, retry_interval)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
- 
-Base = declarative_base() 
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
